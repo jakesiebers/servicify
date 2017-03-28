a3dfc0a6ebf90d43296eea08549a174fd83f0b1f
Jake Siebers <jakesiebers@gmail.com>
2017-03-28 16:19:51 -0700

    Get tests

---
+++ test/test.js
@@ -0,0 +1,63 @@
+
+const assert = require('assert');
+
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+chai.should();
+
+
+// Define the functions
+
+const mathEndpoints = [
+  {
+    method: 'GET',
+    name: 'add',
+    arguments: {
+      a: "number",
+      b: "number",
+    },
+    action: ({ a, b }) => Number(a) + Number(b),
+    description: "add a and b",
+  },
+  {
+    method: 'GET',
+    name: 'subtract',
+    arguments: {
+      a: "number",
+      b: "number",
+    },
+    action: (req, res) => {
+      res.json({
+        data: Number(req.query.a) - Number(req.query.b)
+      });
+    },
+    description: "subtract b from a",
+  }
+];
+
+
+// Make the service server side
+
+const servicify = require('../servicify');
+
+const mathService = servicify(mathEndpoints);
+
+mathService.listen(8080, () => console.log('Listening'));
+
+
+// Consume the service from the client side
+
+const consumeService = require('../consumeService');
+
+const clientSideMathService = consumeService('127.0.0.1', 8080);
+
+
+// Run tests
+
+describe('Servicify', function() {
+  it('Should return 5', () => clientSideMathService.then(math => math.add({ a: 2, b: 3 })).should.eventually.equal(5));
+  it('Should return 5', () => clientSideMathService.run('add', { a: 2, b: 3 }).should.eventually.equal(5));
+  it('Should return 5', () => clientSideMathService.run('subtract', { a: 8, b: 3 }).should.eventually.equal(5));
+});
