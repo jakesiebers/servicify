7d9866addefd5274f9abe685c5dcbf23775d78d6
Jake Siebers <jakesiebers@gmail.com>
2017-05-08 10:20:14 -0700

    Cors support

--- Servicify/Servicify.js
+++ Servicify/Servicify.js
@@ -28,6 +28,13 @@ const Servicify = config => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
+  const corsMiddleware = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  };
+  app.use(corsMiddleware);
+
 
   const errorResponse = (res, err) => {
     res.status(err.statusCode).json({
